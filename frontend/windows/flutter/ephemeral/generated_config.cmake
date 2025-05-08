f766bdca3881d6398fa66e60242039ebece2bb12
Ananya V A <142164935+ananya990@users.noreply.github.com>
2025-05-08 22:43:36 +0530

    Add files via upload

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\data\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\data\\Documents\\MAD Project\\flutter_application_2 new\\flutter_application_2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\data\\Downloads\\flutter"
+  "PROJECT_DIR=D:\\data\\Documents\\MAD Project\\flutter_application_2 new\\flutter_application_2"
+  "FLUTTER_ROOT=D:\\data\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\data\\Documents\\MAD Project\\flutter_application_2 new\\flutter_application_2\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\data\\Documents\\MAD Project\\flutter_application_2 new\\flutter_application_2"
+  "FLUTTER_TARGET=D:\\data\\Documents\\MAD Project\\flutter_application_2 new\\flutter_application_2\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\data\\Documents\\MAD Project\\flutter_application_2 new\\flutter_application_2\\.dart_tool\\package_config.json"
+)
